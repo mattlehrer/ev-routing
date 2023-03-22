784850379702b2be6dad62a483cdbb1126d66113
Matt Lehrer <lehrerm@gmail.com>
2023-03-22 16:35:52 +0100

    install leaflet

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { browser } from '$app/environment';
+	import { LeafletMap, TileLayer } from 'svelte-leafletjs?client';
+	import 'leaflet/dist/leaflet.css';
+
+	const mapOptions = {
+		center: [55.55498175819643, 13.108723370558785],
+		zoom: 11,
+	};
+	const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	const tileLayerOptions = {
+		minZoom: 0,
+		maxZoom: 20,
+		maxNativeZoom: 19,
+		attribution: '© OpenStreetMap contributors',
+	};
+</script>
+
+<div id="map" style="height: 100vh;">
+	{#if browser}
+		<LeafletMap options={mapOptions}>
+			<TileLayer url={tileUrl} options={tileLayerOptions} />
+		</LeafletMap>
+	{/if}
+</div>
