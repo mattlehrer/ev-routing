8a423e3455da5098d59f51c06c93ff20978b0bfe
Matt Lehrer <lehrerm@gmail.com>
2023-03-29 15:55:43 +0200

    better formatting latlng, add reset

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,36 +6,43 @@
 	import 'leaflet/dist/leaflet.css';
 	import { onMount } from 'svelte';
 
-	let origin: string;
-	let destination: string;
+	let origin: string | undefined;
+	let destination: string | undefined;
 	let originLatLng: [number, number];
 	let destinationLatLng: [number, number];
 	let geoJsonData: any;
 
+	function formatLatLng(latlng: LatLng) {
+		return [
+			(latlng.lat as number).toLocaleString(undefined, { maximumFractionDigits: 5 }),
+			(latlng.lng as number).toLocaleString(undefined, { maximumFractionDigits: 5 }),
+		].join(', ');
+	}
+
 	async function handleClick(e: any) {
 		console.log(e.detail.latlng);
 		if (!origin) {
-			origin = [e.detail.latlng.lat, e.detail.latlng.lng].join(', ');
+			origin = formatLatLng(e.detail.latlng);
 			originLatLng = [e.detail.latlng.lat, e.detail.latlng.lng];
 		} else {
+			destination = formatLatLng(e.detail.latlng);
 			destinationLatLng = [e.detail.latlng.lat, e.detail.latlng.lng];
-			destination = [e.detail.latlng.lat, e.detail.latlng.lng].join(', ');
-			const res = await fetch('/api/route', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					origin: originLatLng,
-					destination: destinationLatLng,
-				}),
-			});
-			const data = await res.json();
-			geoJsonData = data.geometry;
-			// console.log(geoJsonData);
-			console.log(data);
-			// geoJsonData = data;
 		}
+		const res = await fetch('/api/route', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				origin: originLatLng,
+				destination: destinationLatLng,
+			}),
+		});
+		const data = await res.json();
+		geoJsonData = data.geometry;
+		// console.log(geoJsonData);
+		console.log(data);
+		// geoJsonData = data;
 	}
 
 	const mapOptions = {
@@ -44,8 +51,8 @@
 		// preferCanvas: true,
 	};
 	// const tileUrl = `https://tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey=${PUBLIC_THUNDERFOREST_API_KEY}`; // shows some elevation details
-	// const tileUrl = `https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=${PUBLIC_THUNDERFOREST_API_KEY}`;
-	const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	const tileUrl = `https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=${PUBLIC_THUNDERFOREST_API_KEY}`;
+	// const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 	const tileLayerOptions = {
 		minZoom: 0,
 		maxZoom: 20,
@@ -128,10 +135,10 @@
 		{/if}
 	</div>
 
-	<div id="route" class="absolute top-0 left-0 z-10 w-80 p-3">
+	<div id="route" class="absolute top-0 left-0 z-10 w-64 p-3">
 		<div class="isolate -space-y-px rounded-md shadow-sm">
 			<div
-				class="relative rounded-md rounded-b-none bg-white px-3 pt-2.5 pb-1.5 ring-1 ring-inset ring-gray-300 focus-within:z-10 focus-within:ring-2 focus-within:ring-indigo-600 "
+				class="relative rounded-md rounded-b-none bg-white px-3 pt-2.5 pb-1.5 ring-1 ring-inset ring-gray-300 focus-within:z-10 focus-within:ring-2 focus-within:ring-indigo-600"
 			>
 				<label for="origin" class="block text-xs font-medium text-gray-900">Origin</label>
 				<input
@@ -140,8 +147,27 @@
 					id="origin"
 					bind:value={origin}
 					class="block w-full border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-					placeholder="Origin"
+					placeholder="Click the map to get started"
 				/>
+				{#if origin}
+					<button
+						class="absolute top-2 right-2 text-red-500 opacity-30"
+						on:click={() => (origin = undefined)}
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							class="h-5 w-5"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+					</button>
+				{/if}
 			</div>
 			<div
 				class="relative rounded-md rounded-t-none bg-white px-3 pt-2.5 pb-1.5 ring-1 ring-inset ring-gray-300 focus-within:z-10 focus-within:ring-2 focus-within:ring-indigo-600"
@@ -153,8 +179,27 @@
 					id="destination"
 					bind:value={destination}
 					class="block w-full border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-					placeholder="Destination"
+					placeholder="Click the map to get started"
 				/>
+				{#if destination}
+					<button
+						class="absolute top-2 right-2 text-red-500 opacity-30"
+						on:click={() => (destination = undefined)}
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							class="h-5 w-5"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+					</button>
+				{/if}
 			</div>
 		</div>
 	</div>
