b55329fc71ba542639b739d88425cc9caa3bebae
Matt Lehrer <lehrerm@gmail.com>
2023-03-30 14:40:39 +0200

    refactor osrm routing

---
+++ src/lib/route.ts
@@ -0,0 +1,31 @@
+import OSRM from '@project-osrm/osrm';
+const osrm = new OSRM({ path: 'osrmdata/sweden-latest.osrm', algorithm: 'MLD' });
+
+export function getRoute(data: { origin: [number, number]; destination: [number, number] }) {
+	return new Promise((resolve, reject) => {
+		console.log({ data });
+		const [olat, olon] = data.origin;
+		const [dlat, dlon] = data.destination;
+		osrm.route(
+			{
+				coordinates: [
+					[olon, olat],
+					[dlon, dlat],
+				],
+				steps: true,
+				overview: 'full',
+				annotations: true,
+				geometries: 'geojson',
+			},
+			(err, result) => {
+				if (err) {
+					console.error(err);
+					reject(err);
+				} else {
+					// console.log(JSON.stringify(result, null, 2));
+					resolve(result?.routes[0]);
+				}
+			},
+		);
+	});
+}
--- src/routes/api/route/+server.ts
+++ src/routes/api/route/+server.ts
@@ -1,35 +1,10 @@
-import OSRM from '@project-osrm/osrm';
+import { getRoute } from '$lib/route';
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-const osrm = new OSRM({ path: 'osrmdata/sweden-latest.osrm', algorithm: 'MLD' });
-
 export const POST = (async ({ request }) => {
 	const data = await request.json();
-	return new Promise((resolve, reject) => {
-		console.log({ data });
-		const [olat, olon] = data.origin;
-		const [dlat, dlon] = data.destination;
-		osrm.route(
-			{
-				coordinates: [
-					[olon, olat],
-					[dlon, dlat],
-				],
-				steps: true,
-				overview: 'full',
-				annotations: true,
-				geometries: 'geojson',
-			},
-			(err, result) => {
-				if (err) {
-					console.error(err);
-					reject(err);
-				} else {
-					// console.log(JSON.stringify(result, null, 2));
-					resolve(json(result?.routes[0]));
-				}
-			},
-		);
-	});
+
+	const route = await getRoute(data);
+	return json(route);
 }) satisfies RequestHandler;
