576d5ce92f0c34ab2a765274426ca0050643bf15
Matt Lehrer <lehrerm@gmail.com>
2023-08-24 10:15:07 +0200

    use nicer tiles

--- src/lib/map.ts
+++ src/lib/map.ts
@@ -1,4 +1,4 @@
-// import { PUBLIC_THUNDERFOREST_API_KEY } from '$env/static/public';
+import { PUBLIC_THUNDERFOREST_API_KEY } from '$env/static/public';
 
 export const mapOptions = {
 	center: [58.83, 14.8],
@@ -6,13 +6,13 @@ export const mapOptions = {
 	// preferCanvas: true,
 };
 // const tileUrl = `https://tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey=${PUBLIC_THUNDERFOREST_API_KEY}`; // shows some elevation details
-// export const tileUrl = `https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=${PUBLIC_THUNDERFOREST_API_KEY}`;
-export const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+export const tileUrl = `https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=${PUBLIC_THUNDERFOREST_API_KEY}`;
+// export const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 export const tileLayerOptions = {
 	minZoom: 0,
 	maxZoom: 20,
 	maxNativeZoom: 19,
-	// attribution:
-	// 	'Maps &copy; <a href="https://www.thunderforest.com">Thunderforest</a>, Data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>',
-	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	attribution:
+		'Maps &copy; <a href="https://www.thunderforest.com">Thunderforest</a>, Data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+	// attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 };
